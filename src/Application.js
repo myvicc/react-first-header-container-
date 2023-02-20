d66d410c891bbef461e0477adce3dac15fed562d
myvicc <dir@acg.co.ua>
2023-02-21 00:33:09 +0200

    added Application.js

---
+++ src/Application.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Box from '@mui/material/Box';
+import Menu from "./Menu";
+
+
+
+export const Application = () => {
+    return (
+    <Box>
+        <Menu />
+    </Box>
+    )
+}
